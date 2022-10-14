6aaebf2faaa13c88c3ad28b360aa523b718b5b67
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-14 10:49:36 -0700

    Created 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num1;
+	int num2;
+	int num3;
+
+	for (num1 = 0; num1 <= 7; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 8; num2++)
+		{
+			for (num3 = num2 + 1; num3 <= 9; num3++)
+			{
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				putchar(num3 + '0');
+
+				if (num1 < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
