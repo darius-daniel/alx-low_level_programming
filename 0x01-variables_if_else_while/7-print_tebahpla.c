4f6431cd5e88868df789e0e9e48f70f6b3c60f3d
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-14 09:20:53 -0700

    Created 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
