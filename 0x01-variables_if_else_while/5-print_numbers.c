64a64e470d7225b37e136c5995bd7e2eb2f97b00
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-14 08:52:03 -0700

    Created 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
