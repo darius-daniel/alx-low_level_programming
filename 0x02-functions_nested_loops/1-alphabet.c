5f9848071e47fc9f8db1d56e8f979133bc24959b
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-18 05:20:24 -0700

    Changed 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include "_putchar.c"
 
 /**
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,21 +5,3 @@ void _putchar(char ch);
 void print_alphabet(void);
 
 void print_alphabet_x10(void);
-
-void _putchar(ch)
-{
-	ch = char ch;
-
-	write (1, ch, 1);
-}
-
-void print_alphabet(void)
-{
-	char ch;
-
-	for (ch = '97'; ch <= '122'; ch++)
-	{
-		_putchar(ch);
-	}
-}
-
