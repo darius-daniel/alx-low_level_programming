45a7bdea920ee91dcbf87c3190637b578dd2bd1d
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-18 07:21:24 -0700

    Created 5-sign.c and updated main.h

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_sign - Test inout parameter to ascertain
+* if number is +ve or -ve
+* @n - The value subject to a test
+* Return - 0 or 1 depending on the input
+*/
+
+int print_sign(int n);
+{
+	int result;
+
+	if (n == 0)
+	{
+		result = 0;
+	}
+	else if (n < 0)
+	{
+		result = -1;
+	}
+	else
+	{
+		result = 1;
+	}
+	return (result);
+}
