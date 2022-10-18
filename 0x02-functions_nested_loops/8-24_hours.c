2837d9d515ff59abe302c5e576cb098b59f3137d
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-18 08:59:13 -0700

    Fixed bug in the second nested loop}l

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,10 +7,20 @@
 void jack_bauer(void)
 {
 	int n1, n2, n3, n4;
+	int x;
 
 	for (n1 = 0; n1 <= 2; n1++)
 	{
-		for (n2 = 0; n2 <= 9; n2++)
+		if (n1 == 2)
+		{
+			x = 3;
+		}
+		else
+		{
+			x = 9;
+		}
+
+		for (n2 = 0; n2 <= x; n2++)
 		{
 			for (n3 = 0; n3 <= 5; n3++)
 			{
