f49bf0ca5c7ab9b36ac2379b2dee76691c077dd7
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-18 15:27:56 -0700

    Made changes to 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			_putchar(n);
+			_putchar(n + '0');
 			n--;
 		}
 	}
@@ -21,7 +21,7 @@ void print_to_98(int n)
 
 		while (i <= n)
 		{
-			_putchar(i);
+			_putchar(i + '0');
 			i++;
 		}
 	}
