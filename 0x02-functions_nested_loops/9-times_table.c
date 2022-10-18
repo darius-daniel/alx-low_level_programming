4a2e2cc87d1ee0b1c4f552a50d85061a9be9b18f
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-18 15:17:32 -0700

    fixed bugs

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ void times_table(void)
 
 			if (col == 0)
 			{
-				_putchar(product + '0')
+				_putchar(product + '0');
 			}
 			if (product < 10 && col != 0)
 			{
@@ -33,7 +33,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar((product / 10) + '0');
-				_putchar((product % 10 + '0');
+				_putchar((product % 10) + '0');
 			}
 		}
 		_putchar('\n');
