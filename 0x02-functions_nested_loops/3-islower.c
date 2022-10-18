bc79e6fb6f33bc1eb4da5c8500d5c9d9206eefd9
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-18 06:20:35 -0700

    Fixed betty errors in 3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
-* _islower - Tests input and returns 0 or 1 
+*_islower - Tests input and returns 0 or 1
 * depending on if the cahracter is loeercase or not
+*
+* @c: Parameter to be tested
+* Return: zero if true; 1 if false
 */
 
 int _islower(int c)
