1cb08a4409831d270b4f282c742c90845332f6df
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-18 05:38:43 -0700

    Made chages to multiple files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,22 +8,13 @@
 
 void print_alphabet(void)
 {
-	char ch;
+	char ch = 'a';
 
-	for (ch = 97; ch <= 122; ch++)
+	while (ch <= 'z')
 	{
 		_putchar(ch);
+		ch++;
 	}
 
 	_putchar('\n');
-}
-
-/**
- * main - Entry point
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
