4a5dff85ed78c343da6005fef14217b445912546
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-18 01:30:36 -0700

    Created 0-putchar.c and updated README.md

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char text[10] = "_putchar\n";
+
+	for (int i = 0; i <= 10; i++)
+	{
+		_putchar(text[i]);
+	}
+
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,3 @@
 main.h - contains several functions and their prototypes
+
+0-putchar.c - a program that prints _putchar, followed by a new line.
