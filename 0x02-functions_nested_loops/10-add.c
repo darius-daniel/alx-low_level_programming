c0e41bcff5e7988706a462d659e06bdf9e4bb106
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-18 09:14:03 -0700

    Created 10-add.c and updated nain.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* add - Calculates the sum of 2 inputs
+* @x: First input
+* @y: Second input
+* Return: returns the sum of x and y
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,3 +15,5 @@ int _abs(int);
 int print_last_digit(int);
 
 void jack_bauer(void);
+
+int add(int, int);
