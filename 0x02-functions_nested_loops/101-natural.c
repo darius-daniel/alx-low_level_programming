9e28f1f447f1016e11a28625fc2154cee75f3a30
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-19 16:30:10 -0700

    Created 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int sum, i;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if ((i % 3 = 0) || (i % 5 = 0))
+		{
+			sum = sum + i;
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
