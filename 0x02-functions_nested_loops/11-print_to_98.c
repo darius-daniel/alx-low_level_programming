c6c8b760b35622981a830f61446d831b0d3fc763
dash2020 <dariusdaniel771@gmail.com>
2022-10-20 08:09:20 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,42 +1,32 @@
-#include "main.h"
+#include <stdio.h>
+#include "holberton.h"
 
 /**
-* print_to_98 - prints a set of numbers from n to 98
-* @n: Maximum value to count to
-*/
-
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
+ */
 void print_to_98(int n)
 {
-	if (n > 98)
+	int i, j;
+
+	if (n <= 98)
 	{
-		while (n >= 98)
+		for (i = n; i <= 98; i++)
 		{
-			_putchar((n/10) + '0');
-			_putchar((n%10) + '0');
-			if (n > 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			n--;
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
-	else
-	{
-		int i = 98;
-
-		while (i <= n)
-		{
-			_putchar((i/10) + '0');
-			_putchar((i%10) + '0');
-			if (i < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			i++;
-		}
-	}
-
-	_putchar('\n');
 }
