d16623d3b66bceed4cc5ec5ca4e3949d7a4e7e1d
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-20 21:45:18 -0700

    Fised bugs

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,13 +8,11 @@
 
 void print_numbers(void)
 {
-	int i = 1;
+	int i;
 
-	while (i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i + '0');
-
-		i++;
 	}
 
 	_putchar('\n');
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,9 @@ void print_most_numbers(void)
 {
 	int i = 0;
 
-	while (i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2 and i != 4)
+		if (i != 2 && i == 4)
 		{
 			_putchar(i + '0');
 		}
