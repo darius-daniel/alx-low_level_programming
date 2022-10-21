aaa8640edd61905096f94840568d6f83511acecc
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-21 06:59:07 -0700

    Finally done with this task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,15 +13,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j < 10)
-			{
-				_putchar(j + '0');
-			}
-			else
+			if (j >= 10)
 			{
 				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
 			}
+			_putchar((j % 10) + '0');
 		}
 
 		_putchar('\n');
