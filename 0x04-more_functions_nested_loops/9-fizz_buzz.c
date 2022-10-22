25e9ed7035bce05f35ff2135b87b33f52ab613b1
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-21 19:12:37 -0700

    Works now

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,24 +16,26 @@ int main(void)
 	{
 		if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else
 		{
-			printf("%d", i);
-		}
-
-		if (i != 100)
-		{
-			printf(" ");
+			if (i == 100)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d ", i);
+			}
 		}
 	}
 
