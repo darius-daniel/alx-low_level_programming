21f388057dab179df775439b5eec89c0f04df45a
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-24 19:21:48 -0700

    Added new line to output

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,15 +11,22 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n == 0 || n == -1)
 	{
-		if (i != n - 1)
+		printf("%d\n", a[0]);
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
-			printf("%d, ", a[i]);
-		}
-		else
-		{
-			printf("%d\n", a[i]);
+			if (i != n - 1)
+			{
+				printf("%d, ", a[i]);
+			}
+			else
+			{
+				printf("%d\n", a[i]);
+			}
 		}
 	}
 }
