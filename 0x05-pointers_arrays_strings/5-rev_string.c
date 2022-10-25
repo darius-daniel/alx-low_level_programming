58fd40c589fddb00959299599ca63cebde8b87ae
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-24 17:24:57 -0700

    Third attemp

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,6 @@
 #include <string.h>
 #include "main.h"
 /**
- * swap_char - swaps the position of 2 chars in a string.
- * @a: first char
- * @b: second char
- */
-
-void swap_char(char *a, char *b)
-{
-	char temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-
-/**
  * rev_string - reverses a string
  * @s: strring to be reversed.
  */
@@ -28,7 +14,9 @@ void rev_string(char *s)
 
 	for (j = strlen(s) - 1; j >= 0; j--)
 	{
-		swap_char(s[i], s[j]);
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
 		i++;
 	}
 }
