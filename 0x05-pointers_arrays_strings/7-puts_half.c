08f0f30146ef391c13e96cbf1ecfa2f408ab01ac
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-24 18:44:12 -0700

    Extra characters from 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,11 +12,11 @@ void puts_half(char *str)
 
 	if (strlen(str) % 2 == 0)
 	{
-		midpoint = strlen(str) / 2;
+		midpoint = (strlen(str) - 2) / 2;
 	}
 	else
 	{
-		midpoint = (strlen(str) - 1) / 2;
+		midpoint = (strlen(str) - 3) / 2;
 	}
 
 	while (str[midpoint] != '\0')
