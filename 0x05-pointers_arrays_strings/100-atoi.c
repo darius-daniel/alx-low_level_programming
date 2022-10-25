73befd3787c29d82e3f319765ae29ccbb5f89919
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-25 03:07:38 -0700

    Created 100-atoi.c. Updated README.md.Updated main.h

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: return the resulting integer
+ */
+
+int _atoi(char *s)
+{
+	int num = atoi(s);
+
+	return (num);
+}
