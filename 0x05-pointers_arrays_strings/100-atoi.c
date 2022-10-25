7a90a8458829add2a1a9bf876bccf972dbabaada
dash2020 <dariusdaniel771@gmail.com>
2022-10-25 23:43:23 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,19 +9,29 @@
 
 int _atoi(char *s)
 {
-	int num = atoi(s);
-	int count = 0;
-	int i;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	for (i = 0; s[i] >= '0' || s[i] <= '9'; i++)
+	while (*(s + count) != '\0')
 	{
-		if (s[i] == '-')
-			count++;
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
 	}
 
-	for (i = 1; i < count; i++)
+	for (i = count - size; i < count; i++)
 	{
-		num = -(num);
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
 	}
-	return (num);
+	return (oi * pn);
 }
