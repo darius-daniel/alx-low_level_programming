9b356286cc0705ba43608cfbb24c223b715016a0
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-27 05:10:01 -0700

    Please work git add .

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,16 @@
 
 void print_number(int n)
 {
+	unsigned int n1 = n;
+
 	if (n < 0)
 	{
-		n = -n;
+		n1 = -n;
 		_putchar('-');
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	if (n1 / 10)
+		print_number(n1 / 10);
 
-	_putchar((n % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
