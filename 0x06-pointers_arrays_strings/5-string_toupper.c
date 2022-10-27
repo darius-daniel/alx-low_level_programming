60e826228c82b5e3ec0fdf350f0be0f055193e22
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-26 19:00:44 -0700

    Fixed issue with 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @s: string to be transformed
  * Return: the transformed string
  */
 
@@ -20,16 +20,4 @@ char *string_toupper(char *s)
 	}
 
 	return (s);
-}
-
-
-int main(void)
-{
-    char str[] = "Look up!\n";
-    char *ptr;
-
-    ptr = string_toupper(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
 }
