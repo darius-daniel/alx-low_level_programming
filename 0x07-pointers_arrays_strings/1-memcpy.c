bc63953e04f89631869daf646bd50532ab8282c5
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-31 19:05:57 -0700

    Added comment for parameter n to 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,6 +4,7 @@
  * _memcpy - copies memory area
  * @dest: destination memory area
  * @src: source memory area
+ * @n: number of bytes to be copied
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
