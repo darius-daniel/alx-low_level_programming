d403f9f4a209b97137a4f1bb54b8128bba02c34e
Darius Daniel <dariusdaniel771@gmail.com>
2022-10-31 19:49:18 -0700

    Sighgit add .!

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the lenth of a prefix substring
+ * @s: initial segment of s
+ * @accept: number of accepted bytes
+ * Return: number of bytes in s which consist only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, bool;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				bool = 0;
+				break;
+			}
+			else
+				bool = 1;
+			j++;
+		}
+		if (bool == 1)
+			break;
+	}
+	return(i);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -3,3 +3,5 @@
 1-memcpy.c - a function that copies memory area
 
 2-strchr.c - a function that locates a character in a string
+
+3-strspn.c - a functiont that get the length of a prefix substring
