7b1806541a4e9c0c9e76761674a5d5323c4b3f5f
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-01 16:17:47 -0700

    Fixed code in 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strspn - gets the lenth of a prefix substring
@@ -31,5 +32,5 @@ unsigned int _strspn(char *s, char *accept)
 		else
 			break;
 	}
-	return (i);
+	return (strspn(s, accept);
 }
