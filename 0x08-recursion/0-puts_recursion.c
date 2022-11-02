c216c9c094e1274bbf13eba3fb237d4e928b9ed4
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-02 03:13:00 -0700

    Corrected errors in file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@ void _puts_recursion(char *s)
 	if (*(s + 1) == '\0')
 	{
 		_putchar(*(s + 0));
+		_putchar('\n');
 	}
 	else
 	{
 		_putchar(*(s + 0));
 		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
 }
