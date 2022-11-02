3d89d21d537e0d93f231feee365ed3b7dc7bd6f0
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-01 17:16:00 -0700

    {

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,17 +10,14 @@ void print_diagsums(int *a, int size)
 {
 	int sum1 = 0;
 	int sum2 = 0;
-	int i, j;
-
-	for (i = 0; i < size; i++)
+	int i;
+	
+	for (i = 0; i < (size * size); i++)
 	{
-		sum1 = sum1 + *(a + i);
+		if (i % (size + 1) == 0)
+			sum1 += *(a + i);
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			sum2 += *(a + i);
 	}
-
-	for (j = size - 1; j >= 0; j--)
-	{
-		sum2 = sum2 + *(a + j);
-	}
-
 	printf("%d, %d\n", sum1, sum2);
 }
