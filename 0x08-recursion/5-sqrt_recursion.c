c9e4fa72ee28e8e1cd9a0733677c69e23147727d
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-02 18:11:08 -0700

    Created 5-sqrt_recursion.c. Updated main.h and README.md

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * getSqrt - finds the natural square root of a number
+  * @n: input number
+  * @c: iterator
+  * Return: the square root of n
+  */
+int getSqrt(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + getSqrt(n, c + 1));
+}
+/**
+  * _sqrt_recursion - returns the natural square root of a number.
+  * @n: number for which the sqrt is calculated
+  * Return: the square root of n, or -1 if n does not have natural square root
+  */
+int _sqrt_recursion(int n)
+{
+	if (n < 1)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else
+		return (power_operation(n, 2));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -7,3 +7,5 @@
 3-factorial.c - a function that returns the factorial of a given number.
 
 4-pow_recursion.c - a function that returns the value of x raised to the power of y.
+
+5-sqrt_recursion.c - a function that returns the natural square root of a number
