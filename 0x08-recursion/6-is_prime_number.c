61f82f2ac248674fd9119f60e06ffb50291ddf24
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-02 18:38:59 -0700

    Created 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+  * is_prime - detects if an input number is a prime number.
+  * @n: input number.
+  * @c: iterator.
+  * Return: 1 if n is a prime number. 0 if n is not a prime number
+  */
+int is_prime(int n, int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	if (c <= 7)
+	{
+		if (c == 2)
+			c += 1;
+		else
+			c += 2;
+		return (0 + is_prime(n, c));
+	}
+}
+/**
+  * is_prime_number - detects if an number is a prime number or not
+  * @n: input number
+  * Return: 1 if n is a prime number. 0 if not
+  */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	else
+		return (is_prime(n, 2));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -9,3 +9,5 @@
 4-pow_recursion.c - a function that returns the value of x raised to the power of y.
 
 5-sqrt_recursion.c - a function that returns the natural square root of a number
+
+6-is_prime_number.c - a function that returns 1 if the input integer is a prime number, otherwise return 0;
