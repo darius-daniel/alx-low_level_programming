ced7fb35e42832d4eb048c850cd38459a96db460
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-02 17:18:47 -0700

    Created a 4-pow_recursion.c. Updated README.md and main.h

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _pow_recursion - returns the value of x raise to the power of y.
+  * @x: base
+  * @y: power
+  * Return: x ** y. If y < 0, return -1.
+  */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return -1;
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -5,3 +5,5 @@
 2-strlen_recursion.c - a function that returns the length of a string.
 
 3-factorial.c - a function that returns the factorial of a given number.
+
+4-pow_recursion.c - a function that returns the value of x raised to the power of y.
