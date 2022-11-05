3ecc4f03d459c184b6e0d8a171093772114399d8
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-05 05:50:52 -0700

    Away we go again

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * Return: 1 if parameter is a digit, and 0 otherwise
+ * @c: parameter
+ *
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - fills memory with a constant byte
+ * @s: pointer to memory area
+ * @b: constant byte to occupy s
+ * @n: number of bytes to be filled
+ * Return: pointer to the memory area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
+
+	return (dest);
+}
