aa6355736edab04846fd523ce63b0bbb07033af6
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-05 09:37:16 -0700

    Finally

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,35 +9,18 @@
   */
 
 int main(int argc, char *argv[])
-<<<<<<< HEAD
 {
-=======
-{	
 	int answer = 0, i = 0;
 	int coins[] = {25, 10, 5, 2, 1};
 	int init_amt;
-	
+
 	if (argc > 1)
 		init_amt = atoi(argv[1]);
->>>>>>> 7e0a82fcab0dababf4f7de9b6a2bec80800f528f
 	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (0);
 	}
-<<<<<<< HEAD
-
-	int init_amt;
-	int answer;
-	int i;
-	int coins[];
-
-	init_amt = atoi(*(argv + 1));
-	answer = 0;
-	i = 0;
-	coins[] = {25, 10, 5, 2, 1};
-=======
->>>>>>> 7e0a82fcab0dababf4f7de9b6a2bec80800f528f
 	if (init_amt < 0)
 		printf("0\n");
 	else
@@ -54,10 +37,6 @@ int main(int argc, char *argv[])
 		}
 		printf("%d\n", answer);
 	}
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 7e0a82fcab0dababf4f7de9b6a2bec80800f528f
 	return (0);
 }
