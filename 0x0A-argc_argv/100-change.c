7e0a82fcab0dababf4f7de9b6a2bec80800f528f
dash2020 <dariusdaniel771@gmail.com>
2022-11-05 17:26:39 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,16 +10,17 @@
 
 int main(int argc, char *argv[])
 {	
+	int answer = 0, i = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+	int init_amt;
+	
+	if (argc > 1)
+		init_amt = atoi(argv[1]);
 	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (0);
 	}
-	
-	int init_amt = atoi(argv[1]);
-	int answer = 0, i = 0;
-	int coins[] = {25, 10, 5, 2, 1};
-	
 	if (init_amt < 0)
 		printf("0\n");
 	else
