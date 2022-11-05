1ad2ce59b01f716b6631c7da4324e1ba94d69c8a
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-05 07:10:00 -0700

    Created 1-args.c. Updated README.md and main.h

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - prints the number of arguments passed into it
+  * @argc: number of arguments passed into main
+  * @argv: list of arguments passed into main
+  * Return: always 0
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,5 @@
 main.h - contains the prototypes of all the functions in this directory
 
 0-whatsmyname.c - a program that prints its name, followed by a new line
+
+1-args.c - a program that prints the number of arguments passed into it
