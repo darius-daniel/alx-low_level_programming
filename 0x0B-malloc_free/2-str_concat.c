0a7264feecb2b152f9c3e9930755f7ee0758f493
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-09 02:42:22 +0100

    Made changes to 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
+
 int _strlen(char *s);
 
 /**
@@ -13,17 +15,17 @@ int _strlen(char *s);
 char *str_concat(char *s1, char *s2)
 {
 	if (s1 == NULL)
-		*s1 = "";
+		s1 = (char *)"";
 	if (s2 == NULL)
-		*s2 = "";
+		s2 = (char *)"";
 
-	int len1, len2, newLen;
+	int len1, len2, newStrLen;
 	char *newStr;
 
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	newStrLen = len1 + len2;
-	newStr = (char *)malloc(sizeof(char) * (newLen + 1));
+	newStr = (char *)malloc(sizeof(char) * (newStrLen + 1));
 
 	if (newStr == NULL)
 		return (NULL);
@@ -50,7 +52,27 @@ int _strlen(char *s)
 {
 	int len;
 
-	for (len = 0; s[len] != '\0')
-		len++;
+	for (len = 0; s[len] != '\0'; len++)
+		;
 	return (len);
+}
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s;
+
+    s = str_concat("Betty ", "Holberton");
+    if (s == NULL)
+    {
+        printf("failed\n");
+        return (1);
+    }
+    printf("%s\n", s);
+    free(s);
+    return (0);
 }
