27f875ec1580d62097a2d13d0cfc7e9ea5d5c3f6
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-09 11:07:21 +0100

    Removed helper main function

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -58,22 +58,3 @@ int _strlen(char *s)
 
 	return(strLen);
 }
-
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(int ac, char *av[])
-{
-    char *s;
-
-    s = argstostr(ac, av);
-    if (s == NULL)
-    {
-        return (1);
-    }
-    printf("%s", s);
-    free(s);
-    return (0);
-}
