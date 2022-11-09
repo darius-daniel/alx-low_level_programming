9b76adb5c5fc12d4a375daf92c93ece93761297f
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-09 01:03:01 +0100

    Rewrote 0-create_arrays.c

---
+++ 0x0B-malloc-free/0-create_array.c
@@ -0,0 +1,78 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * create_array - creates and array of chars, and initializes it with a special
+ * char
+ * @size: size of array
+ * @c: character the array will be initialized to
+ * Return: a char, or NULL if size = 0
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *p[size];
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	p = (char *)malloc(sizeof(c) * size);
+
+	if (cr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		p[i] = c;
+
+	return (p);
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *buffer;
+
+    buffer = create_array(98, 'H');
+    if  (buffer == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
+    simple_print_buffer(buffer, 98);
+    free(buffer);
+    return (0);
+}
