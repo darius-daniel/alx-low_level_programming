93e7faa0807ed4cf86b156eb57f3b13287e82622
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-09 03:43:57 +0100

    Fixed issues in 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -5,23 +5,36 @@
  * alloc_grid - returns a pointer to a 2-D array of integers.
  * @width: how many elements in each sub array
  * @height: how many sub arrays
+ * Return: Pointer to a 2 dimensional array of integers.
 */
 int **alloc_grid(int width, int height)
 {
 	int **arrOfInts, row, col;
 
-	if (width == 0 || height == 0)
+	if (width < 0 || height < 0)
 		return (NULL);
 
 	arrOfInts = malloc(sizeof(int *) * height);
+	if (arrOfInts == NULL)
+	{
+		free(arrOfInts);
+		return (NULL);
+	}
 
 	for (row = 0; row < height; row++)
 	{
 		arrOfInts[row] = (int *)malloc(sizeof(int) * width);
+		if (arrOfInts[row] == NULL)
+		{
+			while (row >= 0)
+			{
+				free(arrOfInts[row]);
+				row--;
+			}
+			free(arrOfInts);
+			return (NULL);
+		}
 	}
-
-	if (arrOfInts == NULL)
-		return (NULL);
 
 	for (row = 0; row < height; row++)
 	{
