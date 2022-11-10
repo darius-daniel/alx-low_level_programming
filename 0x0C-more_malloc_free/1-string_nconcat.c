b3084169a61cebf20b0362406f5105ad19ded017
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-10 17:07:49 +0100

    Removed main function from 2 files

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,14 +41,3 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	return (newStr);
 }
-
-int main(void)
-{
-	char *concat;
-
-	concat = string_nconcat("Best ", "School !!!", 6);
-	printf("%s\n", concat);
-	printf("%ld\n", strlen(concat));
-	free(concat);
-	return (0);
-}
