994a36d5ec5fa6494e6afbbf5d390194ac53bdc5
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-10 17:50:39 +0100

    Nothing to say

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,7 +17,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int newStrLen;
 	int i, j;
 
-	if (n >= strlen(s2))
+	if (n > strlen(s2))
 		n = strlen(s2);
 	if (s1 == NULL)
 		s1 = "";
