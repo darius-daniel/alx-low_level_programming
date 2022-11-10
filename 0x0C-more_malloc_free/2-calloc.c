0934faf5918cb3fe6d0c683c18e0868af661927e
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-11 00:54:08 +0100

    Recreated 2-calloc.c. Created 3-array_range.c and updated both README.md and main.h

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array using malloc and sets it to zero
+ * @nmemb: number of elements in the array
+ * @size: size (bytes) of each element
+ * Return: pointer to the allocated memory
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *arrBlock, i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arrBlock = malloc(size * nmemb);
+	if (arrBlock == NULL)
+		return (NULL);
+
+	for (i = 0; i < (size * nmemb); i++)
+		arrBlock[i] = 0;
+
+	return (arrBlock);
+}
