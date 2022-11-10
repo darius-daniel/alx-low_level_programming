322a778be51c8ee955775b4baf76f5cd8306acc5
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-10 22:56:37 +0100

    Appease betty

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -24,8 +24,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	for (i = 0; i < (nmemb*size); ++i)
-		*(newBlock + i) = (int)0;
+	for (i = 0; i < (nmemb * size); ++i)
+		*(newBlock + i) = 0;
 
 	return (newBlock);
 }
