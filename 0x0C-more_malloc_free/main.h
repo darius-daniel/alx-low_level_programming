89bc0a8e99094c0bb7b51ea54ee353fa8fb7b8ee
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-10 17:19:03 +0100

    Corrected various errors

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,5 +2,5 @@
 #define _MAIN_H_
 
 void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);;
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif
