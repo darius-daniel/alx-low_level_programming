c4a8ebb3de0f6019fdd7a902c0235ff3e96ff7d5
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-14 15:26:19 -0800

    Created 2-main.c and uodated README.md

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - prints the name of the file
+  * @argc: number of command line arguments
+  * @argv: array of commamd line argumemts
+  * Return: 0
+  */
+int main(int argc, char argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -1,3 +1,5 @@
 0-object_like_macro.h - defines a macro named SIZE as an abbreviatiin for the token 1024
 
 1-pi.h - defines a macro named PI as an abbreviation for the token 3.14159265359
+
+2-main.c - prints the name of the file it was compiled from
