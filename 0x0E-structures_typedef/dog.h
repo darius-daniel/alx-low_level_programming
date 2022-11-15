f06793e4407ed2b5d889ed04e31ce1b6528c267a
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-15 15:54:00 -0800

    Made changes to dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,13 @@
   * @owner: dog's ownere
   */
 
-struct dog
+typedef struct
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
