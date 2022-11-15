42caae692a635113ea52cb6c2caad4bd0f7dadb5
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-15 15:36:44 -0800

    Created 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+  * print_dog - prints a dog struct
+  * @d: the dog to be printed
+  * Return: no return
+  */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (!(d->name))
+			printf("Name: (nil)\n");
+		else if (d->name)
+			printf("Name: %s\n", d->name);
+
+		if (!(d->age))
+			printf("Age: (nil)\n");
+		else if (d->name)
+			printf("Age: %f\n", d->age);
+
+		if (!(d->owner))
+			printf("Owner: (nil)\n");
+		else if (d->owner)
+			printf("Owner: %s\n", d->owner);
+	}
+}
