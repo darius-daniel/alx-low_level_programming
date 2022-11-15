8283eaa0a72a9fb7ec92ae23b812126d0b1ace43
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-14 16:40:49 -0800

    Created dog.h and README.md

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+dog.h - defines a new tyoe struct dog
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+  * struct dog - defines a struct tyoe about a dog
+  * @name: dog's name
+  * @age: dog's age
+  * @owner: dog's ownere
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
