50f353b46b59cc914b7e49320896c32beb621562
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-16 15:01:52 -0800

    First commit for this directory

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - prints a name
+  * name: name to be printed
+  * @f: pointer to function
+  * Return: no return
+  */
+
+void print_name(char *name, void(*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+function_pointers.h - contains the prototypes of all functions in this directory
+
+0-print_name.c - a function that prints a name.
