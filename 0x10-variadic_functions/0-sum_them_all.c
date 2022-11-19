46c1eb88e6317a6ecd920f6120c51273d0ede454
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-18 16:44:47 -0800

    Fixed warnings preventing compilatiin

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,11 +8,11 @@
   */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return (0);
-
 	int sum = 0, i, num;
 	va_list nums;
+
+	if (n == 0)
+		return (0);
 
 	va_start(nums, n);
 	for (i = 0; i < n; i++)
