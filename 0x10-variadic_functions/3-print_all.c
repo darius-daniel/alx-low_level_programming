ffa80a158bce2f64c754627d73c9152f83eb79b7
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-18 18:59:15 -0800

    3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,60 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+  * print_all - a function that prints anything
+  * @format: list of types of arguments passed to the function
+  * @c: char
+  * @i: integer
+  * @f: float
+  * @s: char s (print (nil) if string is NULL)
+  * Return: Nothing
+  */
+void print_all(const char * const format, ...)
+{
+	const char type_args[] = "cifs";
+	int i, j = 0, k;
+	char *str;
+	va_list items;
+
+	va_start(items, format);
+	while (format[j] != '\0')
+	{
+		switch (format[j])
+		{
+			case ('c'):
+				printf("%c", va_arg(items, int));
+				break;
+			case ('i'):
+				printf("%d", va_arg(items, int));
+				break;
+			case ('f'):
+				printf("%f", va_arg(items, double));
+				break;
+			case ('s'):
+				str = va_arg(itrms, char *);
+				switch(*str)
+				{
+					case ('\0'):
+						printf("(nil)");
+						break;
+					default:
+						printf("%s", str);
+						break;
+				}
+		}
+		i = 0, k = 0;
+		while (type_args[i] != '\0')
+		{
+			if (type_args[i] == format[j])
+				k = 1;
+			i++;
+		}
+		if (k && format[j + 1] != '\0')
+			printf(", ");
+		j++;
+	}
+	printf("\n");
+	va_end(items);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,5 +5,6 @@ char _putchar(char ch);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
