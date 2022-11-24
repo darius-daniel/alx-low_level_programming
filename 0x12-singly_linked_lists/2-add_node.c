06be6541d93a8ed98cb7aa65dec046b59cbf67bf
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-24 17:49:29 +0100

    Created 2-add_node.c. Updated lists.h and README.md

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: first node in the list
+ * @str: string - (malloc'd string)
+ * Return: address of the new element;
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
+
+/**
+ * _strlen - computes the number of characters are in a string (excluding '\0')
+ * @str: pointer to the string
+ * Return: number of char in the string
+*/
+
+size_t _strlen(const char *str)
+{
+	unsigned int char_count = 0;
+
+	while (*str)
+	{
+		char_count++;
+		str++;
+	}
+	return (char_count);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,6 +21,9 @@ typedef struct list_s
 } list_t;
 
 char _putchar(char c);
+size_t _strlen(const char *str);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+
 #endif
