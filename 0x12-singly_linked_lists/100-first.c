c57c42c172de1bf8db74768176aa464afd1bd508
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-24 21:16:36 +0100

    Fixed output of file 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,5 +6,6 @@
 */
 void __attribute__ ((constructor)) printBeforeMain(void)
 {
-	printf("You're beat! and yet, you must allow, \nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
