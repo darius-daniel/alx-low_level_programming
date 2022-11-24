eae1e7be8fa61810fcb750d727229e20c5f5bc96
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-24 17:55:58 +0100

    add_node now returns NULL if memory allocation fails

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,6 +11,8 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return(NULL);
 	new_node->str = strdup(str);
 	new_node->len = _strlen(str);
 	new_node->next = *head;
