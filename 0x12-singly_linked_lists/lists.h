edbca866d12e2e5ce376bab8de6966fd2a12f41b
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-24 17:06:35 +0100

    Added description for struct list_s

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,13 +5,13 @@
 #include <stdlib.h>
 #include <string.h>
 
- /**
-  * struct list_s - singly linked list
-  * @str: string (malloc'ed string)
-  * @len: length of the string
-  * @next: points to the next node
-  *
-  * Description: singly linked list node structure
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
