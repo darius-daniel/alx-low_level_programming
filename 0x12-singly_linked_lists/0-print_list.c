e5ce2be3c5c230fee9ae3cd5ec294fc63e2a3127
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-24 17:01:46 +0100

    Created and Updated Multiple files.

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: first node in the list
+ * Return: number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	const list_t *temp_node = h;
+	unsigned int count_nodes = 0;
+
+	while (temp_node != NULL)
+	{
+		if (temp_node->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", temp_node->len, temp_node->str);
+		temp_node = temp_node->next;
+		count_nodes++;
+	}
+	return (count_nodes);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,16 @@
 #ifndef _LISTS_H_
-#define _LIST_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
  /**
   * struct list_s - singly linked list
   * @str: string (malloc'ed string)
   * @len: length of the string
   * @next: points to the next node
-  * 
+  *
   * Description: singly linked list node structure
  */
 typedef struct list_s
@@ -16,5 +20,6 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+char _putchar(char c);
 size_t print_list(const list_t *h);
 #endif
