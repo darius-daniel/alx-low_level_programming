e97f9b103271cc92003eea03880be3aa2e2eb0e5
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-24 17:57:20 +0100

    Fixed minor betty error in 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-		return(NULL);
+		return (NULL);
 	new_node->str = strdup(str);
 	new_node->len = _strlen(str);
 	new_node->next = *head;
