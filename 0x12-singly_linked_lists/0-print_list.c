64ab773d8307913cd99d5068ceb6c4ad7aaff64f
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-25 10:34:44 +0100

    Trying new code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,17 @@
 */
 size_t print_list(const list_t *h)
 {
-	const list_t *temp_node = h;
-	size_t count_nodes = 0;
+	 size_t z = 0;
 
-	while (temp_node != NULL)
-	{
-		if (temp_node->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", temp_node->len, temp_node->str);
-		temp_node = temp_node->next;
-		count_nodes++;
-	}
-	return (count_nodes);
+    while (h)
+    {
+        if (!h->str)
+            printf("[0] (nil)");
+        else
+            printf("[%u] %s\n", h->len, h->str);
+        h = h->next;
+        z++;
+    }
+
+    return (z);
 }
