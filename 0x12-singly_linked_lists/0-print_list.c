66ae7c62452dd6fdea5069cfc02194ddb5408de3
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-25 11:03:04 +0100

    Trial and error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,17 @@
 */
 size_t print_list(const list_t *h)
 {
-	 size_t z = 0;
+	size_t z = 0;
 
-    while (h)
-    {
-        if (!h->str)
-            printf("[0] (nil)");
-        else
-            printf("[%u] %s\n", h->len, h->str);
-        h = h->next;
-        z++;
-    }
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		z++;
+	}
 
-    return (z);
+	return (z);
 }
