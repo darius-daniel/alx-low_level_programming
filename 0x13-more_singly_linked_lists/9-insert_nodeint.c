172e8a656a08edb493d3cb3b56617c3ce9e5edd6
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-29 03:30:43 +0100

    Again!

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,16 +36,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			curr = curr->next;
 			count++;
 		}
-		if (idx == get_size(*head))
-		{
-			curr->next = new;
-			new->next = NULL;
-		}
-		else
-		{
-			prev->next = new;
-			new->next = curr;
-		}
+		prev->next = new;
+		new->next = curr;
 	}
 
 	return (*head);
