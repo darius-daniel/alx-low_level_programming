246ba436eae79fcd0f0febbb6ecd568b331af7de
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-30 02:38:18 +0100

    Reverted to code before the '12x' commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *prev, *curr, *new;
 	unsigned int count = 0;
 
-	if ((!*head && idx != 0))
+	if ((!*head && idx != 0) || idx > get_size(*head))
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
