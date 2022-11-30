23c8ebcfcdae22d83c67746fac198b5b2da2df0e
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-30 02:30:20 +0100

    Ninth run at Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,8 +24,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head = new;
+		if (!*head)
+		{
+			new->next = *head;
+			*head = new;
+			(*head)->next = NULL;
+		}
+		else
+		{
+			new->next = *head;
+			*head = new;
+		}
 	}
 	else
 	{
