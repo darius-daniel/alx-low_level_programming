d635bc4c831adc4526e9ddc971b5a60a1a155d67
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-30 02:34:44 +0100

    Tenth try

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,17 +24,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		if (!*head)
-		{
-			new->next = *head;
-			*head = new;
-			(*head)->next = NULL;
-		}
-		else
-		{
-			new->next = *head;
-			*head = new;
-		}
+		new->next = *head;
+		*head = new;
 	}
 	else
 	{
@@ -55,6 +46,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			prev->next = new;
 			new->next = curr;
 		}
+		return (new);
 	}
 
 	return (*head);
