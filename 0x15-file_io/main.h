b95ff3d2909f52cea4d27cb8c12a292fdea7e88d
Darius Daniel <dariusdaniel771@gmail.com>
2022-12-05 19:05:57 -0800

    Another one down

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,8 +4,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 char _putchar (char ch);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
