b2f4cbe116cc6c27fa8c44423d87eb7a57f71a66
Darius Daniel <dariusdaniel771@gmail.com>
2023-02-04 03:16:38 +0100

    Uploading changes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,18 +8,25 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table;
+	hash_node_t **array;
+	unsigned int i;
 
 	new_hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (new_hash_table == NULL)
 		return (NULL);
 
 	new_hash_table->size = size;
-	new_hash_table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t));
-	if (new_hash_table->array == NULL)
+	array = (hash_node_t **)malloc(size * sizeof(hash_node_t));
+	if (array == NULL)
 	{
 		free(new_hash_table);
 		return (NULL);
 	}
+
+	new_hash_table->array = array;
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
 
 	return (new_hash_table);
 }
