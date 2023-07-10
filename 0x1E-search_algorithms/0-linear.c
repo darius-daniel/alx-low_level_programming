90f550d2de60e4e3d03176863f18a00c949acf7f
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-10 10:58:11 +0100

    Fixed Betty style errors

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,16 +9,19 @@
  * Return: if @value not in @array or @array is NULL, return -1.
  * Else, return the first index where @value is located
  */
-int linear_search(int *array, size_t size, int value) {
-  size_t i;
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
 
-  if (array != NULL) {
-	for (i = 0; i < size; i++) {
-	  printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-	  if (array[i] == value)
-		return i;
+	if (array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+				return (i);
+		}
 	}
-  }
 
-  return -1;
+	return (-1);
 }
