6b0a94d97a709b843be776575ac69059f38168cd
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-11 11:20:57 +0100

    Done with many tasks

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,64 @@
+#include "search_algos.h"
+
+/**
+* binary_search - searches for a value in a sorted array of integer using
+* the Binary search algorithm
+* @array: a pointer to the first element of the array to search in
+* @size: the number of elements in @array
+* @value: the value to search for
+* Return: If @value is missing from @array or if @array is NULL, return -1.
+* Else, return index where @value is located
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	if (array != NULL)
+		return (search(array, 0, size - 1, value));
+
+	return (-1);
+}
+
+/**
+* print_list - print a sub_list being searched for a value
+* @array: the array being searched
+* @start: the starting index of the sub-array
+* @stop: the index of the last element of the sub_array
+*/
+void print_list(int *array, size_t start, size_t stop)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = start; i <= stop; i++)
+	{
+		if (i == stop)
+			printf("%d\n", array[i]);
+		else
+			printf("%d, ", array[i]);
+	}
+}
+
+
+/**
+* search - does the actual work of searching
+* @array: the array it is searching in
+* @left: the left-most index of @array
+* @right: the right-most index of @array
+* @value: the value searched for in @array
+*/
+int search(int *array, size_t left, size_t right, int value)
+{
+	int mid = left + (right - left) / 2;
+
+	if (left <= right)
+	{
+		print_list(array, left, right);
+		if (array[mid] == 0)
+			return (mid);
+		else if (array[mid] > value)
+			return (search(array, left, mid - 1, value));
+		else
+			return (search(array, mid + 1, right, value));
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,7 +3,13 @@
 
 #include <stdio.h>
 
-/* Prototypes */
+/* ================== Prototypes =================== */
+/* Helper functions */
+void print_list(int *array, size_t start, size_t stop);
+int search(int *array, size_t left, size_t right, int value);
+
+/* Main funcitons */
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
