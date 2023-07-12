634f4faef305def19bab5024ced5f93bd7f96762
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-12 11:20:59 +0100

    Fixed betty errors in many files

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -43,7 +43,6 @@ int jump_search(int *array, size_t size, int value)
 			if (array[i] == value)
 				return (i);
 		}
-		return (-1);
 	}
 
 	return (-1);
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -24,6 +24,7 @@ typedef struct listint_s
 /* ================== Prototypes =================== */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 
